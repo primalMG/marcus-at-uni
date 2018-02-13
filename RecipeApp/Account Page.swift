72dcd5ff228eb25b571ccc33003b89b2d5f53830
(s) Marcus Gardner <mgardner@PM7-0664-mac.socnet.soc.plymouth.ac.uk>
2018-02-13 17:16:52 +0000

    set up

---
+++ RecipeApp/Account Page.swift
@@ -0,0 +1,9 @@
+//
+//  Account Page.swift
+//  RecipeApp
+//
+//  Created by (s) Marcus Gardner on 13/02/2018.
+//  Copyright © 2018 (s) Marcus Gardner. All rights reserved.
+//
+
+import Foundation
---
+++ RecipeApp/TipsNTricks.swift
@@ -0,0 +1,9 @@
+//
+//  TipsNTricks.swift
+//  RecipeApp
+//
+//  Created by (s) Marcus Gardner on 13/02/2018.
+//  Copyright © 2018 (s) Marcus Gardner. All rights reserved.
+//
+
+import Foundation
