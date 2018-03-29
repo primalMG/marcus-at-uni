c9543ce6ee84ad71e2bd8128dfba33765597475c
(s) Marcus Gardner <mgardner@PM7-0664-mac.socnet.soc.plymouth.ac.uk>
2018-03-29 18:11:33 +0100

    began implementing shoppinglistview

--- RecipeApp/ShoppingListViewController.swift
+++ RecipeApp/ShoppingListViewController.swift
@@ -13,16 +13,19 @@ import FirebaseAuth
 class ShoppingListViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var txtIngredient: UITextField!
     
     var ref: DatabaseReference!
     var databaseHandle: DatabaseHandle!
     var ingredientsArray: [String] = []
     let currUser = Auth.auth().currentUser?.uid
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,20 +33,22 @@ class ShoppingListViewController: UIViewController{
         // Dispose of any resources that can be recreated.
     }
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
     
     
     @IBAction func btnAddIng(_ sender: Any) {
-        
+        let ingKey = ref.child("user").child(self.currUser!).child("ShoppingList").childByAutoId().key
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            if self.currUser != nil {
+                self.ref.child("user").child(self.currUser!).child("ShoppingList").setValue(["\(ingKey)" : self.txtIngredient.text])
+            }else {
+                //redirect to login page...
+            }
+        }
     }
     
-
+    @IBAction func unwindToShoppingList(_ sender: UIStoryboardSegue){
+        
+    }
 
 
 }
